ef565559302113be54cde26faccc2cde2664f833
Milad Khajavi <khajavi@gmail.com>
2025-05-30 08:47:32 +0330

    fix exercise numbering.

--- src/main/scala/zionomicon/solutions/03-the-zio-error-model.scala
+++ src/main/scala/zionomicon/solutions/03-the-zio-error-model.scala
@@ -5,9 +5,9 @@ import zio._
 object TheZIOErrorModel {
 
   /**
-   * Using the appropriate effect constructor, fix the following function so
-   * that it no longer fails with defects when executed. Make a note of how the
-   * inferred return type for the function changes.
+   *   1. Using the appropriate effect constructor, fix the following function
+   *      so that it no longer fails with defects when executed. Make a note of
+   *      how the inferred return type for the function changes.
    */
   object Exercise1 {
 
@@ -16,11 +16,11 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#foldCauseZIO` operator and the `Cause#defects` method,
-   * implement the following function. This function should take the effect,
-   * inspect defects, and if a suitable defect is found, it should recover from
-   * the error with the help of the specified function, which generates a new
-   * success value for such a defect.
+   *   2. Using the `ZIO#foldCauseZIO` operator and the `Cause#defects` method,
+   *      implement the following function. This function should take the
+   *      effect, inspect defects, and if a suitable defect is found, it should
+   *      recover from the error with the help of the specified function, which
+   *      generates a new success value for such a defect.
    */
   object Exercise2 {
 
@@ -37,10 +37,10 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#foldCauseZIO` operator and the `Cause#prettyPrint` method,
-   * implement an operator that takes an effect, and returns a new effect that
-   * logs any failures of the original effect (including errors and defects),
-   * without changing its failure or success value.
+   *   3. Using the `ZIO#foldCauseZIO` operator and the `Cause#prettyPrint`
+   *      method, implement an operator that takes an effect, and returns a new
+   *      effect that logs any failures of the original effect (including errors
+   *      and defects), without changing its failure or success value.
    */
   object Exercise3 {
 
@@ -53,9 +53,9 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#exit` method, which "runs" an effect to an `Exit` value,
-   * implement the following function, which will execute the specified effect
-   * on any failure at all:
+   *   4. Using the `ZIO#exit` method, which "runs" an effect to an `Exit`
+   *      value, implement the following function, which will execute the
+   *      specified effect on any failure at all:
    */
   object Exercise4 {
 
@@ -70,8 +70,9 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#refineOrDie` method, implement the `ioException` function,
-   * which refines the error channel to only include the `IOException` error.
+   *   5. Using the `ZIO#refineOrDie` method, implement the `ioException`
+   *      function, which refines the error channel to only include the
+   *      `IOException` error.
    */
   object Exercise5 {
 
@@ -82,8 +83,8 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#refineToOrDie` method, narrow the error type of the
-   * following effect to just `NumberFormatException`.
+   *   6. Using the `ZIO#refineToOrDie` method, narrow the error type of the
+   *      following effect to just `NumberFormatException`.
    */
   object Exercise6 {
 
@@ -92,9 +93,9 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#foldZIO` method, implement the following two functions,
-   * which make working with `Either` values easier, by shifting the unexpected
-   * case into the error channel (and reversing this shifting).
+   *   7. Using the `ZIO#foldZIO` method, implement the following two functions,
+   *      which make working with `Either` values easier, by shifting the
+   *      unexpected case into the error channel (and reversing this shifting).
    */
   object Exercise7 {
 
@@ -116,9 +117,9 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#foldZIO` method, implement the following two functions,
-   * which make working with `Either` values easier, by shifting the unexpected
-   * case into the error channel (and reversing this shifting).
+   *   8. Using the `ZIO#foldZIO` method, implement the following two functions,
+   *      which make working with `Either` values easier, by shifting the
+   *      unexpected case into the error channel (and reversing this shifting).
    */
   object Exercise8 {
 
@@ -140,7 +141,7 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#sandbox` method, implement the following function.
+   *   9. Using the `ZIO#sandbox` method, implement the following function.
    */
   object Exercise9 {
 
@@ -152,7 +153,8 @@ object TheZIOErrorModel {
   }
 
   /**
-   * Using the `ZIO#foldCauseZIO` method, implement the following function.
+   *   10. Using the `ZIO#foldCauseZIO` method, implement the following
+   *       function.
    */
   object Exercise10 {
     def catchAllCause[R, E1, E2, A](
