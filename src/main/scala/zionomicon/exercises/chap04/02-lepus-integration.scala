e4a9d9c81d568f398ec38456caca7ea7f3b2e02d
Milad Khajavi <khajavi@gmail.com>
2025-05-30 20:59:31 +0330

    lepus integration.

---
+++ src/main/scala/zionomicon/exercises/chap04/02-lepus-integration.scala
@@ -0,0 +1,8 @@
+package zionomicon.exercises.chap04
+
+/**
+ *   2. Write a ZIO program that uses lepus to connect to RabbitMQ server and
+ *      publish arbitrary messages to a queue. Lepus is a purely functional
+ *      scala client for RabbitMQ. You can find the library homepage
+ *      [here](http://lepus.hnaderi.dev/).
+ */
