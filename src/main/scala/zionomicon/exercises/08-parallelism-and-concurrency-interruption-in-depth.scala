ac1590e1d78a59ab535624d72754f15321df77d6
Milad Khajavi <khajavi@gmail.com>
2025-10-04 14:00:48 +0330

    initial work.

---
+++ src/main/scala/zionomicon/exercises/08-parallelism-and-concurrency-interruption-in-depth.scala
@@ -0,0 +1,26 @@
+package zionomicon.exercises
+
+import zio.test.TestAspect._
+import zio.test._
+import zio._
+
+object ParallelismAndConcurrencyInterruptionInDepth extends ZIOSpecDefault {
+
+  override def spec =
+    suite("InterruptionInDepth")(
+      test("interruptible") {
+        for {
+          ref   <- Ref.make(0)
+          latch <- Promise.make[Nothing, Unit]
+          fiber <- ZIO
+                     .uninterruptible(latch.succeed(()) *> ZIO.never)
+                     .ensuring(ref.update(_ + 1))
+                     .forkDaemon
+          _ <- Live.live(
+                 latch.await *> fiber.interrupt.disconnect.timeout(1.second)
+               )
+          value <- ref.get
+        } yield assertTrue(value == 1)
+      } @@ nonFlaky
+    )
+}
